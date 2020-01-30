0b45370a4df2810a5bfc13568ed54343ad66aed1
NicolD PC <nicolgit@users.noreply.github.com>
2020-01-30 14:30:56 +0100

    try-catch exception

--- Controllers/CommitController.cs
+++ Controllers/CommitController.cs
@@ -25,40 +25,48 @@ namespace github_to_lametric.Controllers
         }
 
         private const string ICON = "i2800";
+        private const string ICON_ERROR = "i2493";
 
         [HttpGet]
         public async Task<LametricRootObject> Get([FromQuery]string repository,[FromQuery]string branch, [FromQuery]int last,[FromQuery] string displayname)
         {
-            HttpClient client = new HttpClient();
-            
-            client.DefaultRequestHeaders.Accept .Clear();
-            client.DefaultRequestHeaders.Add("User-Agent", "github-to-lametric");
-
-            var stringTask = client.GetStringAsync($"https://github.com/{repository}/commits/{branch}.atom");
-            var xml = await stringTask;
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(xml);
-
-            string json  = JsonConvert.SerializeXmlNode(doc);
-
-            var atomRootObject =  JsonConvert.DeserializeObject<AtomRootObject>(json);
-            
             var root = new LametricRootObject();
             root.frames = new List<Frame>();
-            
-            for (int i=0; i<last; i++)
+            try
             {
-                if (atomRootObject.feed.entry.Count>=i)
+                HttpClient client = new HttpClient();
+            
+                client.DefaultRequestHeaders.Accept .Clear();
+                client.DefaultRequestHeaders.Add("User-Agent", "github-to-lametric");
+
+                var stringTask = client.GetStringAsync($"https://github.com/{repository}/commits/{branch}.atom");
+                var xml = await stringTask;
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xml);
+
+                string json  = JsonConvert.SerializeXmlNode(doc);
+
+                var atomRootObject =  JsonConvert.DeserializeObject<AtomRootObject>(json);
+                
+                for (int i=0; i<last; i++)
                 {
-                    var f = new Frame();
-                    f.icon = ICON;
-                    f.index = i;
-                    f.text = $"{displayname}: {atomRootObject.feed.entry[i].author.name} have commited '{ cleanTitle(atomRootObject.feed.entry[i].title)}' { makeElapsedString(atomRootObject.feed.entry[i].updated)} ago";
+                    if (atomRootObject.feed.entry.Count>=i)
+                    {
+                        var f = new Frame();
+                        f.icon = ICON;
+                        f.index = i;
+                        f.text = $"{displayname}: {atomRootObject.feed.entry[i].author.name} have commited '{ cleanTitle(atomRootObject.feed.entry[i].title)}' { makeElapsedString(atomRootObject.feed.entry[i].updated)} ago";
 
-                    root.frames.Add(f);
+                        root.frames.Add(f);
+                    }
                 }
-            }
 
+            }
+            catch (Exception e)
+            {
+                root.frames.Add(new Frame(999, ICON_ERROR, e.Message));
+            }
+            
             return root;
         }
 
--- Models/Frame.cs
+++ Models/Frame.cs
@@ -3,6 +3,18 @@ namespace github_to_lametric.Models
 {
     public class Frame
     {
+        public Frame(int index, string icon, string text)
+        {
+            this.index = index;
+            this.icon = icon;
+            this.text = text;
+        }
+        
+        public Frame()
+        {
+            
+        }
+
         public string text { get; set; }
         public string icon { get; set; }
         public int index { get; set; }
